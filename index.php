36c5fb8d2c827fa3bdaa8d4e0506b2b8707e5627
JefradoGG <mfahreza1100@gmail.com>
2025-02-12 08:24:23 +0800

    first commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Travel</title>
+    <title>Co Kreatif</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-success-subtle">
